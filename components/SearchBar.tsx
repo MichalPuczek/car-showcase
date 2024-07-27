479cdb37905fbf35da474c5085983241aa677601
Michal <michal.puczek@gmail.com>
2024-07-27 11:14:55 +0200

    initial commit

---
+++ components/SearchBar.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { SearchManufacturer } from "./";
+
+export default function SearchBar() {
+  // STATE 1
+  const [manufacturer, setManufacturer] = useState("");
+  // STATE 2
+  const [model, setModel] = useState("");
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (manufacturer.trim() === "" && model.trim() === "") {
+      return alert("Please provide some input");
+    }
+
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if (model) {
+      searchParams.set("model", model);
+    } else {
+      searchParams.delete("model");
+    }
+
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer);
+    } else {
+      searchParams.delete("manufacturer");
+    }
+
+    const newPathname = `${
+      window.location.pathname
+    }?${searchParams.toString()}`;
+
+    router.push(newPathname, { scroll: false });
+  };
+
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+          alt="car model"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan..."
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
+    </form>
+  );
+}
+
+function SearchButton({ otherClasses }: { otherClasses: string }) {
+  return (
+    <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+      <Image
+        src="/magnifying-glass.svg"
+        alt="magnifying glass"
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+    </button>
+  );
+}
